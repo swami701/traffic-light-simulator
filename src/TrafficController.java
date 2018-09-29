b7d53abf0cc66a3b9d03698734c39e5c9f9b4e55
Swaminathan Nagarajan <swami701search@gmail.com>
2018-09-30 02:03:07 +0800

    TIMER TASK: Include mock timer task

---
+++ src/TrafficController.java
@@ -0,0 +1,32 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TrafficController {
+    private static final int END_AFTER_MINS = 1;
+    private static final int YELLOW_DURATION = 3;
+    private static final int GOING_STRAIGHT_DURATION = 57;
+    private static final int GOING_RIGHT_DURATION = 67;
+    private int sec;
+    private int min;
+
+    public void simulateTrafficLight() {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(sec++ + 1 + " sec");
+                if (sec % 60 == 0) {
+                    min++;
+                    System.out.println(min + " minute(s)");
+                }
+
+                //
+                if (min >= END_AFTER_MINS) {
+                    System.out.println("Stopping Simulation after " + min + " minutes!");
+                    cancel();
+                }
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 0, 1000);
+    }
+}
--- src/TrafficLightApplication.java
+++ src/TrafficLightApplication.java
@@ -1,5 +1,7 @@
 public class TrafficLightApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        TrafficController trafficController = new TrafficController();
+        trafficController.simulateTrafficLight();
     }
 }
